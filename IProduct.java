3c7130c76f7716a6f4a500329637ab17ce50f5b4
Umit Havare <umithavare16@gmail.com>
2023-07-06 23:30:43 +0300

    product ve Iproduct updated

--- IProduct.java
+++ IProduct.java
@@ -1,3 +1,9 @@
 public interface IProduct {
-
+    public int getId();
+    public String getBrand();
+    public void setBrand(String name);
+    public double getUnitPrice();
+    public void setUnitPrice(double unitPrice);
+    public double getDiscount();
+    void setDiscount(double discount);
 }
--- Product.java
+++ Product.java
@@ -1,2 +1,106 @@
-public class Product {
+public class Product<T> implements IProduct {
+    private int id;
+    private static int nextId = 1;
+    private String brand;
+    private double unitPrice;
+    private double discount;
+    private int stock;
+    private String description;
+    private int memory;
+    private double size;
+    private int ram;
+
+    IProduct product;
+
+    public Product( String brand, double unitPrice, double discount, int stock, String description, int memory, double size, int ram) {
+        this.id = nextId;
+        nextId++;
+        this.brand = brand;
+        this.unitPrice = unitPrice;
+        this.discount = discount;
+        this.stock = stock;
+        this.description = description;
+        this.memory = memory;
+        this.size = size;
+        this.ram = ram;
+    }
+
+    public Product() {
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+    @Override
+    public String getBrand() {
+        return brand;
+    }
+
+    @Override
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    @Override
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public double getDiscount() {
+        return discount;
+    }
+
+    @Override
+    public void setDiscount(double discount) {
+        this.discount = discount;
+
+    }
+
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
 }
